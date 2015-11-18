3bc36c3ff33a1789abb4100be8c8e34a215c53d0
dwclwebadmin <dwclweb@gmail.com>
2015-11-18 11:58:54 +0800

    Design new layout for the school

---
+++ newdesign/header.php
@@ -0,0 +1,5 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Bootstrap 101 Template</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">  
+</head>
